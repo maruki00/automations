3f984c3ab630ac55342f8559c2e1c8eef9a2b3ba
Abdellah Oulahyane <aitbaha48@gmail.com>
2025-04-10 11:26:23 +0100

    Create commit.sh

---
+++ commit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# move it to /usr/bin/commit
+# give it the exec permission by chmod +x /usr/bin/commit
+# type commit in path of your project .
+comment="${1:-commit}"
+echo "The commit is with comment: $comment"

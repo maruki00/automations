c8e608f47198b63d768e93b0b3e5ae538b2536f2
Abdellah Oulahyane <aitbaha48@gmail.com>
2025-04-20 21:57:30 +0100

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -2,6 +2,16 @@
 # move it to /usr/bin/commit
 # give it the exec permission by chmod +x /usr/bin/commit
 # type commit in path of your project .
-comment="${1:-commit}"
-echo "The commit is with comment: $comment"
-git add $(pwd) && git commit -am $comment
+# --------> Old Version
+##comment="${1:-commit}"
+##echo "The commit is with comment: $comment"
+##git add $(pwd) && git commit -am $comment
+#  -------> New Version
+comment="commit"
+if [ $# -eq 1 ] ; then
+    comment=$1
+fi
+echo "Commit with ["$comment"] comment."
+echo "Script executed from: $(pwd)"
+git add $(pwd) && git commit -a -m $comment
+
